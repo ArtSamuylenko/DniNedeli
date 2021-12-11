6487c5a6a0f77516aa72956e522cf82067e1dc29
ArtSamuylenko <89385274775@mail.ru>
2021-12-11 21:51:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace DniNedeli
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите число которое будет соответствовать определенному дню недели:");
+
+                int day = int.Parse(Console.ReadLine());
+                switch (day)
+                {
+                    case 1:
+                        Console.WriteLine("Это понедельник!");
+                        break;
+                    case 2:
+                        Console.WriteLine("Это вторник!");
+                        break;
+                    case 3:
+                        Console.WriteLine("Это среда!");
+                        break;
+                    case 4:
+                        Console.WriteLine("Это четверг!");
+                        break;
+                    case 5:
+                        Console.WriteLine("Это пятница!");
+                        break;
+                    case 6:
+                        Console.WriteLine("Это суббота!");
+                        break;
+                    case 7:
+                        Console.WriteLine("Это воскресенье!");
+                        break;
+                    default:
+                        Console.WriteLine("Это число не соответсвует никакому из дней недели!");
+                        break;
+                }
+            }
+        }
+    }
+}
